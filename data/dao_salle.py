7f3d09b13ad42f4a76d16103723d931a2dccb770
aftisse <300151721@monboreal.ca>
2026-04-18 18:04:51 -0400

    inserer une nouvelle salle avec methode de insert_salle

--- data/dao_salle.py
+++ data/dao_salle.py
@@ -15,3 +15,15 @@ class DataSalle:
             database=config["database"]
         )
         return connexion
+    def insert_salle(self, salle):
+        connexion = self.get_connection()
+        cursor = connexion.cursor()
+        requete = """
+        INSERT INTO salle (codee,descriptions,categorie,capacite)
+        VALUES (%s,%s,%s,%s)
+        """
+        valeurs =(salle.codee,salle.descriptions,salle.categorie,salle.capacite)
+        cursor.execute(requete, valeurs)
+        connexion.commit()
+        cursor.close()
+        connexion.close()
