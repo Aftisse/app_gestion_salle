5e29a714dba404a98b3230a2f25f291238bba982
aftisse <300151721@monboreal.ca>
2026-04-18 19:56:42 -0400

    methode ajouter_salle dans service_salle

--- services/service_salle.py
+++ services/service_salle.py
@@ -3,3 +3,13 @@ from data.dao_salle import DataSalle
 class ServiceSalle:
     def __init__(self):
         self.dao_salle = DataSalle()
+    def ajouter_salle(self,salle):
+        if not salle.codee or not salle.descriptions or not salle.categorie or salle.capacite is None:
+            return False, "Tous les champs obligatoire"
+        if int(salle.capacite) < 1:
+            return False, "la capacite doit etre >= a 1"
+        try:
+            self.dao_salle.insert_salle(salle)
+            return True,"confirmation de l'ajout de salle"
+        except Exception as e:
+            return False, f"Erreur: {e}"
