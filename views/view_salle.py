6b54bd65e047652b5df1d2483e17e5698351199c
aftisse <300151721@monboreal.ca>
2026-04-18 21:23:31 -0400

    Cadre informations de salle

--- views/view_salle.py
+++ views/view_salle.py
@@ -12,3 +12,25 @@ class ViewSalle(ctk.CTk):
 
         self.service_salle = ServiceSalle()
 
+        self.cadreInfo = ctk.CTkFrame(self, corner_radius=10)
+        self.cadreInfo.pack(pady=10, padx=10, fill="x")
+
+        ctk.CTkLabel(self.cadreInfo, text="Code salle").grid(row=0, column=0, padx=10, pady=10)
+        self.entry_code = ctk.CTkEntry(self.cadreInfo)
+        self.entry_code.grid(row=0, column=1, padx=10, pady=10)
+
+        ctk.CTkLabel(self.cadreInfo, text="Description").grid(row=1, column=0, padx=10, pady=10)
+        self.entry_description = ctk.CTkEntry(self.cadreInfo)
+        self.entry_description.grid(row=1, column=1, padx=10, pady=10)
+
+        ctk.CTkLabel(self.cadreInfo, text="Categorie").grid(row=2, column=0, padx=10, pady=10)
+        self.entry_categorie = ctk.CTkEntry(self.cadreInfo)
+        self.entry_categorie.grid(row=2, column=1, padx=10, pady=10)
+
+        ctk.CTkLabel(self.cadreInfo, text="Capacité").grid(row=3, column=0, padx=10, pady=10)
+        self.entry_capacit = ctk.CTkEntry(self.cadreInfo)
+        self.entry_capacit.grid(row=3, column=1, padx=10, pady=10)
+
+
+
+
